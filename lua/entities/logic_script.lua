f3e780a845468c28c98e6103e2c957083922f9fb
Pop4484 <67192138+Pop4484@users.noreply.github.com>
2023-11-12 15:05:37 +0200

    fix logic_script
    im stupid

--- lua/entities/logic_script.lua
+++ lua/entities/logic_script.lua
@@ -9,7 +9,7 @@ include("portal2/portal2_common.lua") // Include the Portal 2 VScript Functions 
 // Go through Portal 2 LUA Ported VScript files, you can add custom ones too!
 local files,dir = file.Find("lua/portal2/vscript/*","GAME")
 for i,script in pairs(files) do
-    include("lua/portal2/vscript/"..script)
+    include("portal2/vscript/"..script)
 end
 
 function ENT:AcceptInput( name, activator, caller, data )
--- lua/portal2/vscript/sp_transition_list.lua
+++ lua/portal2/vscript/sp_transition_list.lua
@@ -225,7 +225,7 @@ function OnPostTransition()
     end
 
 
-end)
+end
 
 
 
